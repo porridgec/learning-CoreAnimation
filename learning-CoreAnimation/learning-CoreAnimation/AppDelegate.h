53fff01f921868a2e5feb459f2e35f750209eadf
Hahn.Chan <porridgec@gmail.com>
2016-03-28 20:24:29 +0800

    init commit

---
+++ learning-CoreAnimation/learning-CoreAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  learning-CoreAnimation
+//
+//  Created by Hahn.Chan on 3/28/16.
+//  Copyright © 2016 Hahn Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
